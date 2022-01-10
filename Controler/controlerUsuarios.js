a71b133044200daeab8a345eff16b3799ed204d6
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2022-01-10 11:03:59 -0300

    Correção: resolução do bug que apagava os dados na atualização do registro

--- Controler/controlerUsuarios.js
+++ Controler/controlerUsuarios.js
@@ -91,61 +91,53 @@ async function id(req, res, id) {
 }
 
 async function alter(req, res, id) {
-  await Schema.findOne({_id: id})
-  .then((registro) =>{
 
-    if(!req.body.nome || !req.body.email)
-    {
-      dados.status = "Atenção"
-      dados.metodo = "PUT"
-      dados.result = { mensagem: "Os campos nome e email são obrigatorios"}
-      res.status(404).json(dados);
+  if(!req.body.nome || !req.body.email)
+  {
+    dados.status = "Atenção"
+    dados.metodo = "PUT"
+    dados.result = { mensagem: "Os campos nome e email são obrigatorios"}
+    res.status(404).json(dados);
 
-    }else
-    {
-        registro.nome = req.body.nome,
-        registro.idade = req.body.idade,
-        registro.data_nasc = req.body.data_nasc,
-        registro.sexo = req.body.sexo,
-        registro.signo = req.body.signo,
-        registro.mae = req.body.mae,
-        registro.pai = req.body.pai,
-        registro.email = req.body.email,
-        registro.cep = req.body.cep,
-        registro.endereco = req.body.endereco,
-        registro.numero = req.body.numero,
-        registro.bairro = req.body.bairro,
-        registro.cidade = req.body.cidade,
-        registro.estado = req.body.estado,
-        registro.telefone = req.body.telefone,
-        registro.celular = req.body.celular
-
-        
-        registro.save().then((dado) => 
-        {
-          dados.status = "Sucesso"
-          dados.metodo = "PUT"
-          dados.result = registro || {mensagem: "Atualizado com sucesso _id:" + id}
-          res.status(200).json(dados);
-
-        }).catch((e) => 
-        {
-          dados.status = "Erro"
-          dados.metodo = "PUT"
-          dados.result = { mensagem: "Os campos nome e email são obrigatorios"}
-          res.status(400).json(dados);
-          res.send({ erro: "Falha ao tentar atualizar os dados" })
-        })
+  }else
+  {
+    const update = {
+      nome: req.body.nome,
+      idade : req.body.idade,
+      data_nasc: req.body.data_nasc,
+      sexo: req.body.sexo,
+      signo: req.body.signo,
+      mae: req.body.mae,
+      pai: req.body.pai,
+      email: req.body.email,
+      cep: req.body.cep,
+      endereco: req.body.endereco,
+      numero: req.body.numero,
+      bairro: req.body.bairro,
+      cidade: req.body.cidade,
+      estado: req.body.estado,
+      telefone: req.body.telefone,
+      celular: req.body.celular
 
     }
 
-  })
-  .catch(() => {
-    dados.status = "Erro"
-    dados.metodo = "PUT"
-    dados.result = {mensagem: "Registro inexistente"}
-    res.status(404).json(dados);
-  })
+    Schema.findOneAndUpdate({ _id: id}, update).then((retorno) => 
+      {
+        dados.status = "Sucesso"
+        dados.metodo = "PUT"
+        dados.result = update || {mensagem: "Atualizado com sucesso _id:" + id}
+        res.status(200).json(dados);
+
+      }).catch((e) => 
+      {
+        dados.status = "Erro"
+        dados.metodo = "PUT"
+        dados.result = { mensagem: "Os campos nome e email são obrigatorios"}
+        res.status(400).json(dados);
+        res.send({ erro: "Falha ao tentar atualizar os dados" })
+      })
+
+  }
 
 }
 
